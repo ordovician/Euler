c063bb2d40d686091f4cd50265d01559d225e0c8
Erik Engheim <erik.engheim@mac.com>
2014-12-23 14:21:02 +0100

    faster way of finding largest prime factor

--- 3.hs
+++ 3.hs
@@ -6,3 +6,19 @@ lpf x | nextFactor == 1  = factor
       | otherwise        = lpf factor
     where factor = largestFactor x
           nextFactor = largestFactor factor 
+
+-- Doesn't work, or at least way slower than fast julia version
+fast' :: Int -> Int -> Int
+fast' 1 1 = 1
+fast' n i | mod n i == 0 = fast' (div n i) 1
+          | i < n - 1    = fast' n (i + 1)
+          | otherwise    = n
+
+fast :: Int -> Int
+fast n = fast' n 2
+
+-- alt n =
+--
+--    where xs = [2, 3 ..]
+--          sf n = find (\x -> 0 == mod n x) xs
+   
--- 3.jl
+++ 3.jl
@@ -15,3 +15,18 @@ function largestprimefactor(number::Int64)
 	end
 	return result
 end
+
+# This is a lot faster than the ones above because we look at the lower
+# numbers first. The idea that we test divide on a low number `i`, so that 
+# div(n, i) is potentially the largest prime factor. If mod(n, i) == 0 it
+# is the largest dividing factor when `i` starts low. But this might not
+# be the largest prime fact. So we find the largest factor of div(n, i) and
+# continue like that.
+function fast(n::Int64)
+   for i in 2:(n-1)
+      if mod(n, i) == 0
+         return fast(div(n, i))
+      end
+   end
+   return n
+end
